38fdcc48418c76197f8f4cab0455e4a25dfaf1de
biancapcr <biancacristina.pocora@gmail.com>
2025-07-14 17:11:23 +0200

    [add] create bikes array

---
+++ 01-bikes/script.js
@@ -0,0 +1,9 @@
+// 1. Creo l'array di oggetti bici
+const bikes = [
+  { nome: 'Bianchi Oltre XR4',        peso: 7.3 },
+  { nome: 'Giant TCR Advanced Pro',   peso: 7.4 },
+  { nome: 'Scott Addict RC 20',       peso: 7.1 },
+  { nome: 'Felt AR FRD',              peso: 6.8 },
+  { nome: 'Colnago V3Rs',             peso: 7.0 },
+  { nome: 'Cervélo R5',               peso: 7.2 }
+];
