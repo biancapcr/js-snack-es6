c9b073a95f25952f95b093bd28b424a79d431caa
biancapcr <biancacristina.pocora@gmail.com>
2025-07-14 17:17:51 +0200

    [add] create array

---
+++ 02-football/script.js
@@ -0,0 +1,9 @@
+// 1. Creo l'array di squadre con solo nome; punti e falli inizializzati a 0
+const teams = [
+  { name: 'Juventus', points: 0, foulsReceived: 0 },
+  { name: 'Milan',    points: 0, foulsReceived: 0 },
+  { name: 'Inter',    points: 0, foulsReceived: 0 },
+  { name: 'Roma',     points: 0, foulsReceived: 0 },
+  { name: 'Napoli',   points: 0, foulsReceived: 0 }
+];
+
